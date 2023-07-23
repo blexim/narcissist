61194b4523c7c9e847828f79d83c7b7a77393fc6
Matt Lewis <blexim@gmail.com>
2023-07-23 21:45:36 +0100

    Initial version of CBMC narcissistic number finder

---
+++ narcissist.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <assert.h>
+
+#define N 3
+#define B 10
+
+int main(void) {
+  int table[B][N+1];
+
+  for (int i = 0; i < B; ++i) {
+    int y = 1;
+    int x = 1;
+
+    for (int j = 0; j < N; ++j) {
+      x *= i;
+    }
+
+    for (int j = 0; j < N+1; ++j) {
+      __CPROVER_assume(table[i][j] == x - i * y);
+      y *= B;
+    }
+  }
+
+  int soln[N+1];
+
+#define NDISALLOWED 6
+
+  int disallowed[NDISALLOWED][N+1] = {
+    {0, 0, 0, 0},
+    {1, 0, 0, 0},
+    {7, 0, 4, 0},
+    {0, 7, 3, 0},
+    {1, 7, 3, 0},
+    {3, 5, 1, 0},
+  };
+
+  for (int i = 0; i < 2; ++i) {
+    int is_same = 1;
+    for (int j = 0; j < N+1; ++j) {
+      int w = soln[j];
+      int v = disallowed[i][j];
+      is_same &= (v == w);
+    }
+    __CPROVER_assume(!is_same);
+  }
+
+  int z = 0;
+
+  for (int i = N; i >= 0; --i) {
+    __CPROVER_assume(soln[i] < 10);
+    __CPROVER_assume(soln[i] >= 0);
+
+    int idx = soln[i];
+    int w = table[idx][i];
+
+    z += w;
+  }
+
+  assert(z != 0);
+}
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cbmc narcissist.c --trace | grep idx=
